cdbbf0850ea9cb263104209fea23a6c21e3b62aa
Alexispavon010 <themaster034@gmail.com>
2024-06-12 01:44:09 -0300

    asd

--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -6,7 +6,6 @@ import { CreateTeacherDto } from './dto/create-teacher.dto';
 import { UpdateTeacherDto } from './dto/update-teacher.dto';
 import { Teacher } from './entities/teacher.entity';
 import { PaginationTeacherDto } from './dto/pagination.dto';
-import { TEACHER_SEED } from './teachers';
 
 @Injectable()
 export class TeacherService {
