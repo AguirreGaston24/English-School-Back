1e88aa5187e091f7926672dc7e3ea7085855a83a
Alexispavon010 <themaster034@gmail.com>
2024-08-08 02:39:42 -0300

    add timestamps

--- src/student/entities/student.entity.ts
+++ src/student/entities/student.entity.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
-@Schema()
+@Schema({ timestamps: true })
 export class Students extends Document {
   @Prop() firstname: string;
   @Prop() lastname: string;
