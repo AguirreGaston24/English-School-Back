f0c3f4c067e8d2ab2d2afae3b97bece615d0edc6
Alexispavon010 <themaster034@gmail.com>
2024-09-25 15:20:27 -0300

    add student interface

---
+++ src/payment/interfaces/student.interface.ts
@@ -0,0 +1,24 @@
+export interface IStudent {
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  country: string;
+  city: string;
+  address: string;
+  district: string;
+  dni: string;
+  school: string;
+  birthdate: Date;
+  tutor: string;
+  tutor_occupation: string;
+  tutor_phone: string;
+  tutor_address: string;
+  tutor_district: string;
+  createdAt: Date;
+  updatedAt: Date;
+  group: string;
+  level: string;
+  teacher: string;
+  birth_date: Date;
+}
